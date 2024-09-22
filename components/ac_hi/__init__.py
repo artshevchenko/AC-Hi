88621ed631de5175636f95b24daf3bd2cca0ff50
Artem Shevchenko <9002056+artshevchenko@users.noreply.github.com>
2024-09-22 16:55:30 +0300

    fix init

--- components/ac_hi/__init__.py
+++ components/ac_hi/__init__.py
@@ -13,7 +13,8 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ACHi),
 }).extend(uart.UART_DEVICE_SCHEMA)
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], cg.get_variable(config[uart.CONF_UART_ID]))
-    yield cg.register_component(var, config)
-    yield uart.register_uart_device(var, config)
+async def to_code(config):
+    uart_component = await cg.get_variable(config[uart.CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID], uart_component)
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
